0930abdbc879c2acca9867d7764ba31a340921ba
Rogier Slag <rogier.slag@gmail.com>
2018-02-07 11:56:16 +0100

    Improve API with endpoints and allow filtering by query params

--- index.js
+++ index.js
@@ -5,15 +5,106 @@ const nagiosParser = require('nagios-status-parser');
 
 const app = express();
 
-app.get('/', async (req, res) => {
-	try {
-		const nagiosData = await readFile('/opt/nagios/status.dat', 'utf8');
-		const parsed = nagiosParser(nagiosData);
-		res.json(parsed);
-	} catch (e) {
-		res.status(500).end(e);
-		console.error(e);
+async function getNagiosData() {
+	const nagiosData = await readFile('/opt/nagios/status.dat', 'utf8');
+	return nagiosParser(nagiosData);
+}
+
+function mapState(input) {
+	switch (input.toUpperCase()) {
+		case 'CRITICAL':
+			return 2;
+		case 'WARNING':
+			return 1;
+		case 'OK':
+			return 0;
+		case 'UNKNOWN':
+			return 3;
+		default:
+			throw new Error('Requested state not supported');
 	}
+}
+
+function toBoolean(input) {
+	switch (input.toLowerCase()) {
+		case 'true':
+		case '1':
+			return 1;
+		case 'false':
+		case '0':
+			return 0;
+		default:
+			throw new Error('Requested state not supported');
+	}
+}
+
+const filters = {
+	state : (input, val) => {
+		const desiredState = mapState(val);
+		return input.filter(e => e.current_state === desiredState);
+	},
+	flapping : (input, val) => {
+		const desiredState = toBoolean(val);
+		return input.filter(e => e.is_flapping === desiredState);
+	}
+};
+
+function filterByQuery(input, query) {
+	if (!query) {
+		return input;
+	}
+
+	return Object.keys(query).reduce((acc, key) => {
+		const value = query[key];
+		const applicableFilter = filters[key];
+		if (!applicableFilter) {
+			return acc;
+		}
+		return applicableFilter(acc, value);
+	}, input)
+}
+
+app.get('/', async (req, res) => {
+	res.json(await getNagiosData());
+});
+
+app.get('/info', async (req, res) => {
+	const data = await getNagiosData();
+	res.json(data.info);
+});
+app.get('/program', async (req, res) => {
+	const data = await getNagiosData();
+	res.json(data.programstatus);
+});
+
+app.get('/hosts/:host/services', async (req, res, next) => {
+	const host = req.params.host;
+	const data = await getNagiosData();
+	res.json(filterByQuery(data.servicestatus.filter(e => e.host_name === host), req.query));
+});
+app.get('/hosts/:host', async (req, res, next) => {
+	const host = req.params.host;
+	const data = await getNagiosData();
+	res.json(data.hoststatus.filter(e => e.host_name === host));
+});
+app.get('/hosts', async (req, res) => {
+	const data = await getNagiosData();
+	res.json(filterByQuery(filterByQuery(data.hoststatus), req.query));
+});
+
+app.get('/services/:service', async (req, res, next) => {
+	const service = req.params.service;
+	const data = await getNagiosData();
+	res.json(filterByQuery(data.servicestatus.filter(e => e.service_description === service), req.query));
+});
+app.get('/services', async (req, res) => {
+	const data = await getNagiosData();
+	res.json(filterByQuery(data.servicestatus, req.query));
+});
+
+app.get('/contacts', async (req, res) => {
+	const data = await getNagiosData();
+	res.json(data.contactstatus);
 });
 
 app.listen(8080, () => {
